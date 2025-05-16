e4912a5bc1c69387af580069c64023a77ba01672
Jorge Gallardo <jorgito899@gmail.com>
2025-05-15 23:30:38 -0400

    feature: Se agrega la sección de tabla con su tabla correspondiente con fetch del API

--- app-mobile/app/(tabs)/_layout.tsx
+++ app-mobile/app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="table"
+        options={{
+          title: 'Table',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="tablecells.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
